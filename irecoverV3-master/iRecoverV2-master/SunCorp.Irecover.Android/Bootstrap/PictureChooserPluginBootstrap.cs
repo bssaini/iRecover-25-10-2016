fefc7d8840a63715610ff3e6fd573ed48d39fbc8
bssaini <1saini.bhupinder@gmail.com>
2016-10-28 18:00:01 +1000

    D1

--- irecoverV3-master/iRecoverV2-master/SunCorp.Irecover.Android/Bootstrap/PictureChooserPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace SunCorp.Irecover.Android.Bootstrap
-{
-    public class PictureChooserPluginBootstrap
-       : MvxPluginBootstrapAction<MvvmCross.Plugins.PictureChooser.PluginLoader>
-    {
-    }
-}
--- irecoverV3-master/iRecoverV2-master/SunCorp.Irecover.Android/Bootstrap/VisibilityPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace SunCorp.Irecover.Android.Bootstrap
-{
-    public class VisibilityPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
-    {
-    }
-}
